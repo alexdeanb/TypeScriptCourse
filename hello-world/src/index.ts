7e99a31c589fb9479bf5c384433b9015237c3c20
Alex Bishop <alexb98@gmail.com>
2023-09-18 14:45:20 -0500

    Intersection Types

--- hello-world/src/index.ts
+++ hello-world/src/index.ts
@@ -1,11 +1,14 @@
-let employee: {
-    readonly id: number,
-    name: string,
-    retire: (date: Date) => void
-} = {
-    id: 1,
-    name: "Mosh",
-    retire: (date: Date) => {
-        console.log(date)
-    }
-};
+type Draggable = {
+    drag: () => void
+}
+
+type Resizable = {
+    resize: () => void
+}
+
+type UIWidget = Draggable & Resizable;
+
+let textBox: UIWidget = {
+    drag: () => {},
+    resize: () => {}
+}
