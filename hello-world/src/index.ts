f5635d9fe16887132d6530378bf605fc6eacbbfe
Alex Bishop <alexb98@gmail.com>
2023-09-19 09:32:51 -0500

    Advanced Types Excercises Complete

--- hello-world/src/index.ts
+++ hello-world/src/index.ts
@@ -1,14 +1,44 @@
-type Draggable = {
-    drag: () => void
+//Excercise One
+type User = {
+    name: string,
+    age: number,
+    occupation?: string
 }
 
-type Resizable = {
-    resize: () => void
+
+let users = [
+    {
+        name: 'John Smith',
+        age: 30,
+        occupation: "Software engineer"
+    },
+    {
+        name: 'Kate Muller',
+        age: 28
+    }
+]
+
+//Excercise Two
+type Bird ={
+    fly: () => void
+}
+type Fish = {
+    swim: () => void
 }
 
-type UIWidget = Draggable & Resizable;
+type Pet = Bird | Fish
 
-let textBox: UIWidget = {
-    drag: () => {},
-    resize: () => {}
-}
+//Excercise Three
+type weekDay = "Monday" | "Tuesday" | "Wednesday" | "Thursday" | "Friday" | "Saturday" | "Sunday"
+
+//Excercise Four
+//Simplify the following
+let user = getUser();   
+console.log(user && user.address ? user.address.street: undefined);
+//Simplified
+let user = getUser();
+console.log(user?.address?.street)
+//Simplify the Following
+let x = foo !== null && foo !== undefined ? foo : bar();
+//Simplified
+let x = foo ?? bar();
