2e0119f1b83d39b8c53a883e79826b49abaf5559
Alex Bishop <alexb98@gmail.com>
2023-10-04 09:52:25 -0500

    Interfaces complete.

--- hello-world/dist/index.js
+++ hello-world/dist/index.js
@@ -1,18 +1,2 @@
 "use strict";
-class Shape {
-    constructor(color) {
-        this.color = color;
-    }
-}
-class Circle extends Shape {
-    constructor(radius, color) {
-        super(color);
-        this.radius = radius;
-    }
-    render() {
-        console.log("Rendering a Circle");
-    }
-}
-let shape = new Circle(1, "red");
-shape.render();
 //# sourceMappingURL=index.js.map
--- hello-world/src/index.ts
+++ hello-world/src/index.ts
@@ -1,19 +1,30 @@
-abstract class Shape {
-    constructor(public color: string){}
+// abstract class Calendar {
+//     constructor(public name: string){}
 
-    abstract render(): void;
+//     abstract addEvent(): void;
+//     abstract removeEvent(): void;
+// }
+
+
+interface ICalendar {
+    name: string;
+    addEvent(): void;
+    removeEvent(): void;
 }
 
-class Circle extends Shape {
-    constructor(public radius: number, color: string){
-        super(color)
-    }
 
-    override render(): void {
-        console.log("Rendering a Circle");
+interface CloudCalendar extends ICalendar {
+    sync(): void;
+}
+
+class GoogleCalendar implements ICalendar {
+    constructor(public name: string) {}
+
+    addEvent(): void {
+        throw new Error("Method not implemented")
+    }
+    removeEvent(): void {
+        throw new Error("Method not implemented")
     }
 }
 
-
-let shape = new Circle(1,"red");
-shape.render();
