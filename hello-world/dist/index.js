cad4cb8465d7f8482c90d2c63afef7c129fcea7a
Alex Bishop <alexb98@gmail.com>
2023-10-05 12:15:07 -0500

    Generic Constraints

--- hello-world/dist/index.js
+++ hello-world/dist/index.js
@@ -1,2 +1,13 @@
 "use strict";
+class Person {
+    constructor(name) {
+        this.name = name;
+    }
+}
+class Customer extends Person {
+}
+function echo(value) {
+    return value;
+}
+echo(new Customer("A"));
 //# sourceMappingURL=index.js.map
